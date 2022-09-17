f0f360cdf7175bf78ba2ddb70a15815e6036e1fd
mkusz <9740637+mkusz@users.noreply.github.com>
2022-09-17 21:38:00 +0000

    Update from https://github.com/mkusz/testerembyc/commit/eb60323506f14d584d7311282a0152a8674f5906

--- presentations/tester-manualny-to-za-malo/plugin/math/plugin.js
+++ presentations/tester-manualny-to-za-malo/plugin/math/plugin.js
@@ -1,15 +1,4 @@
-import {KaTeX} from "./katex";
-import {MathJax2} from "./mathjax2";
-import {MathJax3} from "./mathjax3";
-
-const defaultTypesetter = MathJax2;
-
-/*!
+import{KaTeX as t}from"./katex";import{MathJax2 as e}from"./mathjax2";import{MathJax3 as a}from"./mathjax3";let defaultTypesetter=e;/*!
  * This plugin is a wrapper for the MathJax2,
  * MathJax3 and KaTeX typesetter plugins.
- */
-export default Plugin = Object.assign( defaultTypesetter(), {
-	KaTeX,
-	MathJax2,
-	MathJax3
-} );
+ */ export default Plugin=Object.assign(defaultTypesetter(),{KaTeX:t,MathJax2:e,MathJax3:a});
